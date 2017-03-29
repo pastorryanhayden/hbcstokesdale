d40f3e9fa990f0946ad466a5dff536f2782073cb
Ryan Hayden <pastorryanhayden@gmail.com>
2017-03-28 22:23:43 -0500

    Made thumbnail plugin

--- _plugins/rmagick.rb
+++ _plugins/rmagick.rb
@@ -1,1 +1,13 @@
-require "RMagick"
+require "rmagick"
+
+Jekyll::Hooks.register :pages, :post_render do |p|
+	# IF there is a gallery
+	if p.data["gallery"]
+		# For every item in the gallery
+		p.data["gallery"].each do |img|
+			infile = img['gallery_image'].sub(/^\//,"")
+			outfile = infile.sub(/(\.\w+)$/, "_thumb\\1")
+			Magick::Image.read(infile).first.resize_to_fill(250,250).write(outfile)
+		end
+	end
+end
