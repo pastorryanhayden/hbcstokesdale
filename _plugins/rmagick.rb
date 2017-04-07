63776e241c1d376e3005a5e2751e1a3ab766b2aa
Ryan Hayden <pastorryanhayden@gmail.com>
2017-04-06 19:12:45 -0500

    added gallery plugin v 1

--- _plugins/rmagick.rb
+++ _plugins/rmagick.rb
@@ -5,9 +5,14 @@ Jekyll::Hooks.register :pages, :post_render do |p|
 	if p.data["gallery"]
 		# For every item in the gallery
 		p.data["gallery"].each do |img|
+			# set the variable infile to be the gallery image minus any initial slash
 			infile = img['gallery_image'].sub(/^\//,"")
+			# create an outfile that substitues the . for _thumb.
 			outfile = infile.sub(/(\.\w+)$/, "_thumb\\1")
-			Magick::Image.read(infile).first.resize_to_fill(250,250).write(outfile)
+			# take the infile (first because some images like gifs actually contain multiple files) copy it as a 250,250 thumbnail and write it using the outfile name
+			img = Magick::Image.read(infile).first
+			thumb = img.resize_to_fill(250,250)
+			thumb.write(outfile)
 		end
 	end
 end
